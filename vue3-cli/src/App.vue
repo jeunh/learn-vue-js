3f165240217ee4b44240cda4916e145b05c27253
jeunh <gkwldms1218@gmail.com>
2023-11-14 22:30:26 +0900

    Vue 싱글 파일 컴포넌트

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader :appTitle="message" @change="changeMessage"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+  },
+  data() {
+    return {
+      message: '앱 헤더 컴포넌트'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = '변경됨';
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
